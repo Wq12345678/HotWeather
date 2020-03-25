a4b68eb708a9f930b3174ce1d88e95ceecc1a3c0
Wq12345678 <1150008791@qq.com>
2020-03-25 11:29:15 +0000

    Add fragment

--- app/src/androidTest/java/com/coolweather/android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/coolweather/android/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.coolweather.coolweather;
+package com.coolweather.android;
 
 import android.content.Context;
 
--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,8 +1,10 @@
-package com.coolweather.coolweather;
+package com.coolweather.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.coolweather.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
